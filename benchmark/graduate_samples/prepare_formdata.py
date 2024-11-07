1814f4ba105e1dbd7c445e0655fb3e801103e9e9
james burgess <jamesburgess@DN0a22a65e.SUNet>
2024-11-06 17:32:49 -0800

    form creation pipeline

---
+++ benchmark/graduate_samples/prepare_formdata.py
@@ -0,0 +1,126 @@
+"""
+python -m ipdb benchmark/graduate_samples/prepare_formdata.py
+
+Load the source csv, choose the `key_question`s, log them, Put them into a new csv
+"""
+import ipdb
+import sys
+import json
+from pathlib import Path
+import pandas as pd
+import ast
+import os
+from PIL import Image
+import shutil
+
+from benchmark.refine_bot.run_experiments import _download_csv
+from benchmark.refine_bot.bot import _stringify_mcq_for_logging
+from benchmark.refine_bot.run_eval import _get_filenames_from_key
+
+dir_results_parent = Path(__file__).parent / "results" / Path(__file__).stem
+dir_results_parent.mkdir(exist_ok=True, parents=True)
+
+lookup_dfs = {
+    'nov5_dspy_full__eval_seed0':
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjs_xqcIYnxcbIx_BUqpkcJS_D5eBWOs7yipC-WEXsN1PGkc-krImgaRZVdCuVKJKTFzL0snAVz39b/pub?gid=2369810&single=true&output=csv"
+}
+with open("benchmark/data/formdata_0/2_lookup_question_to_person.json",
+          'r') as fp:
+    lookup_question_to_person = json.load(fp)
+cache_images = {}
+
+df_people = pd.read_csv("benchmark/data/formdata_0/2_df_people.csv")
+lookup_person = dict(zip(df_people['key_person'], df_people['Your name']))
+# people who dropped out
+skip_key_person = [1, 2, 3, 5, 11, 16, 19]
+
+
+def get_df_from_key(key, overwrite=False):
+    """ 
+    It also creates the results folder using the same naming convention 
+    """
+    url = lookup_dfs[key]
+    dir_csvs = Path(__file__).parent / "results/prepare_formdata"
+    dir_csvs.mkdir(exist_ok=True)
+    f_csv = dir_csvs / f"IN_{key}.csv"
+    f_csv_out = dir_csvs / f"OUT_{key}.csv"
+    dir_people_out = dir_csvs / f"OUT_{key}"
+    dir_people_out.mkdir(exist_ok=True)
+    _download_csv(url, f_csv, overwrite=overwrite)
+    df = pd.read_csv(f_csv)
+    df = df.set_index('key_question')
+
+    return df, f_csv_out, dir_people_out
+
+
+def nov5_choose():
+    """ """
+    df_all, f_csv_out, dir_people_out = get_df_from_key(key='nov5_dspy_full__eval_seed0')
+
+    # apply criteria 
+    df = df_all[df_all["wrong_3_times"] == 1].copy()
+    df = df[df['code'].isin(["SUCCESS_NO_CHANGE", "SUCCESS_REWRITE"])].copy()
+
+    # save 
+    df.to_csv(f_csv_out)
+
+    # save the per-person stuff 
+    create_form_datasets_perperson(df, dir_people_out)
+
+    ipdb.set_trace()
+    pass
+
+
+def create_form_datasets_perperson(df, dir_people_out):
+    # people lookup
+    df.loc[:, 'key_person'] = [
+        lookup_question_to_person[str(k)] for k in df['question_key']
+    ]
+
+    df = pd.merge(df, df_people, on='key_person').copy()
+
+    for key_person in df['key_person'].unique():
+        if key_person in skip_key_person:
+            continue
+
+        # person directory
+        person_name = lookup_person[key_person]
+        dir_results_person = dir_people_out / f"p{key_person}_{person_name.replace(' ' ,'_')}"
+        dir_results_person.mkdir(exist_ok=True)
+
+        df_subset = df[df['key_person'] == key_person].copy()
+
+        mcq_strs = []
+        for i, row in df_subset.iterrows():
+            choices = ast.literal_eval(row["choices"])
+            mcq_str = _stringify_mcq_for_logging(row["question"],
+                                                 choices["choices"],
+                                                 choices["correct_index"])
+            correct_answer = choices["choices"][choices["correct_index"]]
+            mcq_str += f"\n\nCorrect answer: {correct_answer}"
+            mcq_strs.append(mcq_str)
+
+        df_subset['mcq_str'] = mcq_strs
+
+        cols = ['question_key', 'key_image', 'mcq_str', 'description_question_answer']
+
+        df_save = df_subset[cols]
+        print(f"{len(df_subset)}\t", person_name)
+
+        key_imgs = df_subset['key_image'].values
+        dir_img_grids = "analysis_scripts/results/20241103_create_images_folders/imgs_grids"
+        for key_img in key_imgs:
+            f_stem = f"grid_{int(key_img):03d}.png"
+            f_src = f"{dir_img_grids}/{f_stem}"
+            f_tgt = f"{dir_results_person}/{f_stem}"
+            shutil.copy(f_src, f_tgt)
+
+        df_save.to_csv(f"{dir_results_person}/data.csv")
+
+
+if __name__ == "__main__":
+    nov5_choose()
+    ipdb.set_trace()
+    pass
+
+
